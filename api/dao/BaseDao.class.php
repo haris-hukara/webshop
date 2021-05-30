89aeec484baa7d1e08671d731d071be092b0b08d
haris-hukara <haris.hukara@stu.ibu.edu.ba>
2021-05-30 18:45:21 +0200

    basic setup

---
+++ api/dao/BaseDao.class.php
@@ -0,0 +1,30 @@
+<?php
+class BaseDao{
+
+ public function __construct(){
+ 
+
+    }
+
+    public function insert(){
+        
+    }
+
+    public function update(){
+        
+    }
+
+    public function query(){
+        
+    }
+
+    public function query_unique(){
+        
+    }
+
+
+
+
+
+}
+?>

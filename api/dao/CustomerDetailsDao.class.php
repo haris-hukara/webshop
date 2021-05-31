80ef8a63a3d4453c4cc37e5d1f37b0c265d1e658
haris-hukara <haris.hukara@stu.ibu.edu.ba>
2021-05-31 20:25:08 +0200

    Adding Customer Details Dao

--- api/dao/CustomerDetailsDao.class.php
+++ api/dao/CustomerDetailsDao.class.php
@@ -1,11 +1,19 @@
 <?php 
+require_once dirname(__FILE__)."/BaseDao.class.php";
 
 class CustomerDetailsDao extends BaseDao{
 
-    public function add_account($account){
-        
+    public function add_customer_details($customerDetails){
+        return $this->insert("customer_details", $customerDetails);
     }
-
+    
+    public function update_customer_details($id, $customerDetails){
+        $this->update("customer_details", $id, $customerDetails);
+    }
+    
+    public function get_customer_details($id){
+        return $this->query_unique("SELECT * FROM customer_details WHERE id = :id ", ["id" => $id]); 
+    }
 
 
 }
--- api/test.php
+++ api/test.php
@@ -4,19 +4,26 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once dirname(__FILE__)."/dao/UserAccountDao.class.php";
+require_once dirname(__FILE__)."/dao/CustomerDetailsDao.class.php";
 
-$user_dao = new UserDao();
+$dao = new CustomerDetailsDao();
 
 // $user = $user_dao->get_user_by_email("haris@email.ba");
 
-$user1 = [
-    "email" => "haris122@mail.com",
-    "password" => "1234444424",
-    "customer_details_id" => 1
+$details = [
+    "name" => "Ime",
+    "surname" => "prezime",
+    "email" => "mail@mail.com",
+    "phone_number" => "123 123 123",
+    "city_id" => 1,
+    "zip_code" => "71000",
+    "address" => "adresa stanovanja 1"
 ];
 
 // $user_dao->add_user($user);
-$user = $user_dao->add_user($user1);
+$user = $dao->get_customer_details(2);
+
+print_r($user);
 
 
 ?>

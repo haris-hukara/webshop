ca05af25c8209a3b59ca2021da6aae9d68e30f37
haris-hukara <haris.hukara@stu.ibu.edu.ba>
2021-06-01 22:49:24 +0200

    add basic routes

--- api/index.php
+++ api/index.php
@@ -7,16 +7,29 @@ error_reporting(E_ALL);
 require dirname(__FILE__).'/dao/UserAccountDao.class.php';
 require dirname(__FILE__).'/../vendor/autoload.php';
 
+Flight::register('userAccountDao', 'UserAccountDao');
 
 Flight::route('/', function(){
     echo 'hello world!';
 });
 
-Flight::route('/account', function(){
-    $dao = new UserAccountDao();
-    $accounts = $dao->get_all(0,10);
-    Flight::json($accounts);
-    
+Flight::route('GET /account', function(){
+    Flight::json(Flight::userAccountDao()->get_all(0,10));
+});
+
+Flight::route('GET /account/@id', function($id){
+    Flight::json(Flight::userAccountDao()->get_by_id($id));
+});
+
+Flight::route('POST /account', function(){
+    $request = Flight::request()->data->getdata();
+    flight::json(Flight::userAccountDao()->add($request));
+});
+
+Flight::route('PUT /account/@id', function($id){
+    $request = Flight::request()->data->getdata();
+    Flight::userAccountDao()->update($id, $request);
+    flight::json(Flight::userAccountDao()->get_by_id($id));
 });
 
 Flight::start();

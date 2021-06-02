f60a2745a5163a92add1cd6af1896d0436177ce9
haris-hukara <haris.hukara@stu.ibu.edu.ba>
2021-06-02 21:01:40 +0200

    User registration added

--- api/index.php
+++ api/index.php
@@ -3,9 +3,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
 require_once dirname(__FILE__).'/../vendor/autoload.php';
+
+/* include classes */
 require_once dirname(__FILE__).'/services/UserAccountService.class.php';
+require_once dirname(__FILE__).'/services/UserDetailsService.class.php';
 
 
 /* utility function for reading params from URL */
@@ -18,10 +20,12 @@ Flight::map('query', function($name, $default_value = NULL){
 
 /* register Bussiness Logic layer services */
 Flight::register('userAccountService', 'UserAccountService');
+Flight::register('userDetailsService', 'UserDetailsService');
 
 
 /* include routes */
 require_once dirname(__FILE__).'/routes/userAccount.php';
+require_once dirname(__FILE__).'/routes/userDetails.php';
 
 
 Flight::start();
---
+++ api/services/UserDetailsService.class.php
@@ -0,0 +1,28 @@
+<?php 
+require_once dirname(__FILE__)."/BaseService.class.php";
+require_once dirname(__FILE__)."/../dao/UserDetailsDao.class.php";
+require_once dirname(__FILE__)."/../dao/UserAccountDao.class.php";
+
+class UserDetailsService extends BaseService{
+
+    public function __construct(){
+     $this->dao = new UserDetailsDao();   
+    }
+
+    /* add override */
+    public function add($userDetails){
+      if(!isset($userDetails['name'])) throw new Exception("Name is missing");
+      if(!isset($userDetails['surname'])) throw new Exception("Surname is missing");
+      if(!isset($userDetails['email'])) throw new Exception("Email is missing");
+      if(!isset($userDetails['phone_number'])) throw new Exception("Phone is missing");
+      if(!isset($userDetails['city'])) throw new Exception("City is missing");
+      if(!isset($userDetails['zip_code'])) throw new Exception("Zip Code is missing");
+      if(!isset($userDetails['address'])) throw new Exception("Address is missing");
+      
+  return parent::add($userDetails);
+  }
+
+
+
+}
+?>

a419e2624d0baf9c086fa46c43a30fdd808f2aee
haris-hukara <haris.hukara@stu.ibu.edu.ba>
2021-06-25 14:20:06 +0200

    adding security

--- api/services/UserAccountService.class.php
+++ api/services/UserAccountService.class.php
@@ -30,7 +30,18 @@ class UserAccountService extends BaseService{
     public function forgot($userAccount){
       $db_user = $this->dao->get_user_by_email($userAccount['email']);
       if(!isset($db_user['id'])) throw new Exception("User doesn't exist", 400);
-      $db_user = $this->update($db_user['id'], ['token' => md5(random_bytes(16)), 'token_created_at' => date(Config::DATE_FORMAT)]);
+      
+      $time_difference = strtotime(date(Config::DATE_FORMAT)) 
+                       - strtotime($db_user['token_created_at']);
+     
+     // token can not be created more then once in 10 mins ( 600sec -> 10 mins)
+      if ($time_difference < 600 ) throw new Exception("Be patient token is on the way", 400);
+      
+      $db_user = $this->update( $db_user['id'], 
+                                ['token' => md5(random_bytes(16)),
+                                'token_created_at' => date(Config::DATE_FORMAT)]
+                              );
+      
       $this->smtpClient->send_recovery_token($db_user);
     }
 
@@ -39,12 +50,15 @@ class UserAccountService extends BaseService{
 
       if(!isset($db_user['id'])) throw new Exception("Invalid token", 400);
       
-      $time_difference = strtotime(date(Config::DATE_FORMAT)) - strtotime($db_user['token_created_at']);
- 
-      if ($time_difference > 60 ) throw new Exception("Token expired", 400);
+      $time_difference = strtotime(date(Config::DATE_FORMAT)) 
+                       - strtotime($db_user['token_created_at']);
 
-      $this->dao->update($db_user['id'],['password' => md5($userAccount ['password']), 'token' => NULL ]);
+      if ($time_difference > 600 ) throw new Exception("Token expired", 400);
 
+      $this->dao->update( $db_user['id'],
+                          ['password' => md5($userAccount ['password']),
+                          'token' => NULL ]
+                        );
     }
 
     public function register($userAccount){

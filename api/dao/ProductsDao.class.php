56b9da21d118fd6869cb9d32e5e2242169c5ddc0
haris-hukara <haris.hukara@stu.ibu.edu.ba>
2021-08-15 14:53:11 +0200

    fixing search for product

--- api/dao/ProductsDao.class.php
+++ api/dao/ProductsDao.class.php
@@ -8,7 +8,7 @@ class ProductsDao extends BaseDao{
     }
 
                      
- public function get_products($search, $offset, $limit, $order = "-id", $category = NULL){
+ public function get_products($search, $offset, $limit, $order = "-id", $category =""){
 
     switch (substr($order, 0, 1)){
         case '-': $order_direction = 'ASC'; break;
@@ -19,22 +19,33 @@ class ProductsDao extends BaseDao{
     $params = [];
     $params["search"] = $search;
     
-    $query = "SELECT p.id,
-                     p.name, 
-                     p.unit_price,
-                     p.image_link, 
-                     p.gender_category,
-                     ps.name AS 'category'
+    $query = "SELECT   p.id,
+                       p.name, 
+                       ps.name AS 'category',
+                       p.gender_category,                     
+                       p.unit_price,
+                       p.image_link
                FROM products p
                JOIN product_subcategory ps ON p.subcategory_id = ps.id  
-               WHERE LOWER(p.name) LIKE CONCAT('%', :search, '%') ";
+               WHERE LOWER(p.name) LIKE CONCAT('%', :search, '%')";
 
-    if ($category){
+    if ($category != ""){
         $query .= " AND LOWER(ps.name) LIKE CONCAT('%', :category, '%')";
         $params["category"] = strtolower($category);
     }
-        return $this->query($query,$params);
+        
+        $order = substr($order, 1);
+        
+        if( strtolower($order) == "category"){
+            $order = "ps.name";
+        }else{
+            $order = "p.".$order;
+        }
 
+        $query .= "ORDER BY ${order} ${order_direction}
+                   LIMIT ${limit} OFFSET ${offset}";
+
+        return $this->query($query,$params);
  }
 
 

87a0f1ce8058997d877dcd17f08ba556180196cb
haris-hukara <haris.hukara@stu.ibu.edu.ba>
2021-09-18 11:13:56 +0200

    password change added

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -2,7 +2,7 @@ function jsonize_form(selector){
     var data = $(selector).serializeArray();
     var form_data = {};
     for (let i = 0; i < data.length; i++) {
-        form_data[data[i].name] = data[i].value;
+      form_data[data[i].name] = data[i].value;
     }
     return form_data;
 }
